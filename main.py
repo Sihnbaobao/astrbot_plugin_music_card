7ae5d5735870b5dadc05f6de83eef443aadb0746
弑魂暴暴 <140788757+Sihnbaobao@users.noreply.github.com>
2026-07-14 00:38:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from astrbot.api.star import Star
-from astrbot.api import register
 from astrbot.api.event import filter, AstrMessageEvent
-
-from .utils.parser import parse_music_url
+from astrbot.api.star import Star, register
 
 
 @register(
-    "music_card",
-    "your_name",
-    "音乐链接转卡片",
+    "astrbot_plugin_music_card",
+    "Sihnbaobao",
+    "QQ音乐网易云音乐链接转音乐卡片",
     "0.1.0"
 )
 class MusicCardPlugin(Star):
@@ -18,38 +15,11 @@ class MusicCardPlugin(Star):
 
 
     @filter.on_message()
-    async def handle_music_link(
-        self,
-        event: AstrMessageEvent
-    ):
+    async def music_card(self, event: AstrMessageEvent):
 
         msg = event.message_str
 
-        music = parse_music_url(msg)
-
-        if not music:
-            return
-
-
-        try:
-
-            await event.bot.call_action(
-                "send_music",
-                **{
-                    "user_id":
-                    event.get_sender_id(),
-
-                    "type":
-                    music["type"],
-
-                    "id":
-                    music["id"]
-                }
-            )
-
-
-        except Exception as e:
-
-            self.context.logger.error(
-                f"音乐卡片发送失败:{e}"
+        if "music.163.com" in msg or "y.qq.com" in msg:
+            yield event.plain_result(
+                "检测到音乐链接"
             )
