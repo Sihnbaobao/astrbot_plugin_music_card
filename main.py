b1ee8a3cbbcb4cce29449354fc230ff3cdd1b211
弑魂暴暴 <140788757+Sihnbaobao@users.noreply.github.com>
2026-07-14 01:50:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ import json
 from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Star, register
 
+from utils.qqmusic import parse_qq_music
+
+
 
 @register(
     "astrbot_plugin_music_card",
     "Sihnbaobao",
     "QQ音乐网易云音乐链接转换音乐卡片",
-    "0.1.3"
+    "0.1.4"
 )
 class MusicCardPlugin(Star):
 
+
     def __init__(self, context):
+
         super().__init__(context)
+
 
 
     # =========================
@@ -27,9 +33,9 @@ class MusicCardPlugin(Star):
         message
     ):
 
-        # 群聊
 
         if event.message_obj.group_id:
+
 
             await event.bot.api.call_action(
                 "send_group_msg",
@@ -38,9 +44,8 @@ class MusicCardPlugin(Star):
             )
 
 
-        # 私聊
-
         else:
+
 
             await event.bot.api.call_action(
                 "send_private_msg",
@@ -51,7 +56,7 @@ class MusicCardPlugin(Star):
 
 
     # =========================
-    # 网易云音乐
+    # 网易云
     # =========================
 
     async def send_163(
@@ -60,20 +65,28 @@ class MusicCardPlugin(Star):
         song_id
     ):
 
-        msg = [
+
+        message = [
+
             {
                 "type": "music",
+
                 "data": {
+
                     "type": "163",
+
                     "id": song_id
+
                 }
+
             }
+
         ]
 
 
         await self.send_music(
             event,
-            msg
+            message
         )
 
 
@@ -85,47 +98,64 @@ class MusicCardPlugin(Star):
     async def send_qq(
         self,
         event,
-        song_mid
+        songid=None,
+        songmid=None
     ):
 
 
-        jump_url = (
-            "https://i.y.qq.com/v8/playsong.html"
-            "?platform=11"
-            "&appshare=android_qq"
-            "&appversion=20040008"
-            f"&songmid={song_mid}"
-            "&type=0"
-            "&appsongtype=1"
-            "&_wv=1"
-            "&source=qq"
-            "&ADTAG=qfshare"
-        )
+        if songid:
+
+
+            jump_url = (
+                "https://i.y.qq.com/v8/playsong.html?"
+                f"songid={songid}"
+                "&songtype=0"
+                "#webchat_redirect"
+            )
+
+
+        else:
+
+
+            jump_url = (
+                "https://i.y.qq.com/v8/playsong.html?"
+                f"songmid={songmid}"
+            )
+
 
 
         card = {
 
+
             "app": "com.tencent.music",
+
 
             "view": "news",
 
+
             "ver": "1.0.0.0",
+
 
             "prompt": "[分享]QQ音乐",
 
+
             "meta": {
+
 
                 "news": {
 
+
                     "title": "QQ音乐",
+
 
                     "desc": "QQ音乐歌曲",
 
+
                     "jumpUrl": jump_url,
 
-                    "preview": "",
 
                     "tag": "QQ音乐"
+
 
                 }
 
@@ -135,7 +165,7 @@ class MusicCardPlugin(Star):
 
 
 
-        msg = [
+        message = [
 
             {
 
@@ -155,9 +185,10 @@ class MusicCardPlugin(Star):
         ]
 
 
+
         await self.send_music(
             event,
-            msg
+            message
         )
 
 
@@ -180,9 +211,7 @@ class MusicCardPlugin(Star):
 
 
 
-        # -----------------
         # 网易云
-        # -----------------
 
         if "music.163.com" in text:
 
@@ -195,12 +224,10 @@ class MusicCardPlugin(Star):
 
             if result:
 
-                song_id = result.group(1)
-
 
                 await self.send_163(
                     event,
-                    song_id
+                    result.group(1)
                 )
 
 
@@ -210,27 +237,43 @@ class MusicCardPlugin(Star):
 
 
 
-        # -----------------
         # QQ音乐
-        # -----------------
-
-        if "y.qq.com" in text:
 
 
-            result = re.search(
-                r"songDetail/([A-Za-z0-9]+)",
-                text
-            )
+        if (
+
+            "y.qq.com" in text
+
+            or
+
+            "c6.y.qq.com" in text
+
+        ):
 
 
-            if result:
+            qq = await parse_qq_music(text)
 
-                song_mid = result.group(1)
+
+
+            if (
+
+                qq["songid"]
+
+                or
+
+                qq["songmid"]
+
+            ):
 
 
                 await self.send_qq(
+
                     event,
-                    song_mid
+
+                    songid=qq["songid"],
+
+                    songmid=qq["songmid"]
+
                 )
 
 
