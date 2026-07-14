4f268b3ec59f198663fc70b80434623643e9deda
弑魂暴暴 <140788757+Sihnbaobao@users.noreply.github.com>
2026-07-15 02:44:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -216,7 +216,7 @@ class MusicCardPlugin(Star):
 
                     "audio":
                     qq.get(
-                        "url",
+                        "audio",
                         ""
                     ),
 
