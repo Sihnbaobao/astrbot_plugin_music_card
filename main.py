35e1718cdf26b96bbd5c877fe93ce533ff5ce8f3
Sihnbaobao <1756758977@qq.com>
2026-07-19 02:56:08 +0800

    restore zh->jp song name hint in search results

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ class MusicCardPlugin(Star):
             f'歌名:{s["name"]} 歌手:{s["artist"]} ID:{s["id"]}'
             for s in results
         ]
-        return "\n".join(lines)
+        return "\n".join(lines) + "\n\n(中文歌名可能以日语显示,如'魔法'='まほう')"
 
     @filter.llm_tool(name="send_song_card")
     async def send_song_card(self, event: AstrMessageEvent, song_id: str):
